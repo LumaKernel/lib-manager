deb81fd0516b7032cf976172263bf56ad9074c0f
Luma <tomorinao.info@gmail.com>
2018-08-21 18:55:07 +0900

    setting for bin

--- index.mjs
+++ index.mjs
@@ -1,3 +1,3 @@
 #!/bin/sh 
-":" //# comment; exec /usr/bin/env node --experimental-modules "$0" "$@"
+":" //# comment; exec /usr/bin/env node --experimental-modules --no-warnings "$0" "$@"
 import("./cli")
