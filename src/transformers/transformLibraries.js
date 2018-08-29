ccabf821a03ba73a270afc5f2b74ec728e820304
Luma <tomorinao.info@gmail.com>
2018-08-29 14:29:08 +0900

    logging

--- src/transformers/transformLibraries.js
+++ src/transformers/transformLibraries.js
@@ -14,16 +14,22 @@ export default async function transformLibraries (config, libs, templates) {
 
 async function dfs (config, libs, templates, key) {
   const now = libs[key]
+  const name = now.name
   if (now.processing) throw `[transformLibraries] ${key} : cannot include cycle`
   now.processing = true
   let codes = [now.code, now.refactored, now.enclosed]
   now['import'].forEach(el => {
-    if (!templates[el.name]) throw `template ${el.name} not found`
+    if (!templates[el.name]) throw `${name} : template ${el.name} not found`
     codes[1] = codes[1].replace(hash(el.id), `// @import ${el.name}\n${templates[el.name].code}\n// @@`)
   })
   await now.require.forEachAsync(async el => {
-    if (!libs[el.name].finished) await dfs(config, libs, templates, el.name, libs[el.name])
-    codes = codes.map(code => code.replace(hash(el.id), libs[el.name].enclosed))
+    if (libs[el.name]) {
+      if (!libs[el.name].finished) await dfs(config, libs, templates, el.name, libs[el.name])
+      codes = codes.map(code => code.replace(hash(el.id), libs[el.name].enclosed))
+    } else {
+      console.log(`${name} : ${el.name} was not found`)
+      codes = codes.map(code => code.replace(hash(el.id), el.old))
+    }
   })
   now.code = await format(codes[0].trim(), config)
   now.refactored = await format(codes[1], config)
